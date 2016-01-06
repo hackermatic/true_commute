fbeaeb7ce496bc90e88bd3b387a91f0017f8e179
hackermatic <hackermatic@users.noreply.github.com>
2016-01-05 21:26:32 -0600

    Packaging skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(name='true_commute',
+      version='0.1.0',
+      description='Calculate your true commute time and cost',
+      packages=['true_commute'],
+      author='hackermatic',
+      author_email='me@hackermatic.com',
+      url='https://github.com/hackermatic/true-commute'
+      )
